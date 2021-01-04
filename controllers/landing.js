3770de9526eb7a9424a336e09f5752ed0a5349ad
Paul Thomas <paulthomas2107@gmail.com>
2021-01-04 18:37:49 +0000

    Fixes

--- controllers/landing.js
+++ controllers/landing.js
@@ -13,19 +13,42 @@ exports.submit_lead = function (req, res, next) {
 };
 
 exports.show_leads = function (req, res, next) {
-  models.Lead.findAll().then((leads) => {
+  return models.Lead.findAll().then((leads) => {
     res.render("landing", { title: "Express", leads: leads });
   });
 };
 
 exports.show_lead = function (req, res, next) {
-  models.Lead.findOne()
-    .then({
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id,
+    },
+  }).then((lead) => {
+    res.render("lead", { lead: lead });
+  });
+};
+
+exports.show_edit_lead = function (req, res, next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id,
+    },
+  }).then((lead) => {
+    res.render("lead/edit_lead", { lead: lead });
+  });
+};
+
+exports.edit_lead = function (req, res, next) {
+  return models.Lead.update(
+    {
+      email: req.body.lead_email,
+    },
+    {
       where: {
         id: req.params.lead_id,
       },
-    })
-    .then((lead) => {
-      res.render("lead", { lead: lead });
-    });
+    }
+  ).then((result) => {
+    res.redirect("/lead/" + req.params.lead_id);
+  });
 };
